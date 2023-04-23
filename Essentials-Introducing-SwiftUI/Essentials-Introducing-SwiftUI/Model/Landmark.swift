7c8594f93c6d46c689597fa5edebe7b0bf901828
Chamsol Kim <kcsol1005@gmail.com>
2023-04-23 09:51:49 +0900

    SwiftUI Essentials: Building Lists and Navigation

---
+++ Essentials-Introducing-SwiftUI/Essentials-Introducing-SwiftUI/Model/Landmark.swift
@@ -0,0 +1,33 @@
+//
+//  Landmark.swift
+//  Essentials-Introducing-SwiftUI
+//
+//  Created by chamsol kim on 2023/04/23.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        .init(latitude: coordinates.latitude, longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
---
+++ Essentials-Introducing-SwiftUI/Essentials-Introducing-SwiftUI/Views/LandmarkList.swift
@@ -0,0 +1,32 @@
+//
+//  LandmarkList.swift
+//  Essentials-Introducing-SwiftUI
+//
+//  Created by chamsol kim on 2023/04/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+        }
+    }
+}
