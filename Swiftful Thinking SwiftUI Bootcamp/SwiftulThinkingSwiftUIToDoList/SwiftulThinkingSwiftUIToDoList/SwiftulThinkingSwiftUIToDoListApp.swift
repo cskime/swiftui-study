5962c4829c7cef854c26ae8d92bfa6f1068d5dc4
Chamsol Kim <kcsol1005@gmail.com>
2023-06-16 15:50:26 +0900

    Create Todo List Beginner

---
+++ Swiftful Thinking SwiftUI Bootcamp/SwiftulThinkingSwiftUIToDoList/SwiftulThinkingSwiftUIToDoList/SwiftulThinkingSwiftUIToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftulThinkingSwiftUIToDoListApp.swift
+//  SwiftulThinkingSwiftUIToDoList
+//
+//  Created by chamsol kim on 2023/06/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftulThinkingSwiftUIToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
