4a751ec5437223e5626acd026bd9d9626616851b
Chamsol Kim <kcsol1005@gmail.com>
2023-06-29 21:20:11 +0900

    Config: Kavsoft example project 추가
    Youtube: https://www.youtube.com/@Kavsoft

---
+++ KavsoftSwiftUIExample/KavsoftSwiftUIExample/KavsoftSwiftUIExampleApp.swift
@@ -0,0 +1,20 @@
+//
+//  KavsoftSwiftUIExampleApp.swift
+//  KavsoftSwiftUIExample
+//
+//  Created by chamsol kim on 2023/06/29.
+//
+
+import SwiftUI
+
+@main
+struct KavsoftSwiftUIExampleApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
